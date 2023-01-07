08f3af48ef8738111e5c7e6cb800f4ff989e8cf4
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-01-07 14:02:10 +0300

    create new branch

--- src/main/java/ru/netology/Radio/Radio.java
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -1,18 +1,45 @@
 package ru.netology.Radio;
 
 public class Radio {
-    private int StationNumber;
-    private int SoundVolume;
+    private int MinNumberStations = 0;
+    private int MaxNumberStation = 9;
+    private int StationNumber = MinNumberStations;
+    private int MinSoundVolume = 0;
+    private int MaxSoundVolume = 100;
+    private int SoundVolume = MinSoundVolume;
+
+    public Radio(int MinNumberStation, int MaxNumberStation) {
+        this.MinNumberStations = MinNumberStation;
+        this.MaxNumberStation = MaxNumberStation;
+        this.StationNumber = MinNumberStation;
+        this.MinSoundVolume = MinSoundVolume;
+        this.MaxSoundVolume = MaxSoundVolume;
+        this.SoundVolume = MinSoundVolume;
+
+    }
+
+    public Radio(int size) {
+        MaxNumberStation = MinNumberStations + size;
+        MaxSoundVolume = MinSoundVolume + size;
+    }
 
     public int getStationNumber() {
         return StationNumber;
     }
 
+    public int getMinNumberStations() {
+        return MinNumberStations;
+    }
+
+    public int getMaxNumberStation() {
+        return MaxNumberStation;
+    }
+
     public void setStationNumber(int StationNumber) {
-        if (StationNumber < 0) {
+        if (this.StationNumber < 0) {
             return;
         }
-        if (StationNumber > 9) {
+        if (this.StationNumber > 9) {
             return;
         }
         this.StationNumber = StationNumber;
@@ -22,18 +49,26 @@ public class Radio {
         return SoundVolume;
     }
 
+    public int getMinSoundVolume() {
+        return MinNumberStations;
+    }
+
+    public int getMaxSoundVolume() {
+        return MaxSoundVolume;
+    }
+
     public void setSoundVolume(int SoundVolume) {
-        if (SoundVolume < 0) {
+        if (this.SoundVolume < 0) {
             return;
         }
-        if (SoundVolume > 10) {
+        if (this.SoundVolume > 100) {
             return;
         }
         this.SoundVolume = SoundVolume;
     }
 
     public void InCreaseVolume() {
-        if (SoundVolume < 10) {
+        if (SoundVolume < 100) {
             SoundVolume = SoundVolume + 1;
         }
     }
